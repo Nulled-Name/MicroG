fde7a95ec58825789fae2cffc66e003e101da54c
Nulled_Name <in.shadow1931@gmail.com>
2026-01-29 13:14:03 -0300

    more debbugging info

--- customize.sh
+++ customize.sh
@@ -1,9 +1,10 @@
 ui_print "Magisk version: "$MAGISK_VER
 ui_print "Magisk API: "$MAGISK_VER_CODE
-ui_print "boot mode: "$BOOTMODE
+ui_print "Magisk app: "$BOOTMODE
 ui_print "Module path: "$MODPATH
 ui_print "temp path: "$TMPDIR
 ui_print "zip path: "$ZIPFILE
 ui_print "arch: "$ARCH
 ui_print "Is 64 bit: "$IS64BIT
 ui_print "android version: "$API
+ui_print "android sdk: "$SDK
