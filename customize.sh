865fad7c6b37ad809b0cb6b00b9317501fece062
Nulled_Name <in.shadow1931@gmail.com>
2026-01-29 12:59:27 -0300

    edit customize for debugging purposes

--- customize.sh
+++ customize.sh
@@ -1,4 +1,9 @@
-echo '(Echo1): '$MODPATH
-echo "(Echo2): "$MODPATH
-ui_print '(UI_Print1): '$MODPATH
-ui_print "(UI_Print2): "$MODPATH
+ui_print "Magisk version: "$MAGISK_VER
+ui_print "Magisk API: "$MAGISK_VER_CODE
+ui_print "boot mode: "$BOOTMODE
+ui_print "Module path: "$MODPATH
+ui_print "temp path: "$TMPDIR
+ui_print "zip path: "$ZIPFILE
+ui_print "arch: "$ARCH
+ui_print "Is 64 bit: "$IS64BIT
+ui_print "android version: "$API
